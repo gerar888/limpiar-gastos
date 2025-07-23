ba8339ff31a084c47af743497e1021234ced2143
gerar888 <72981628+gerar888@users.noreply.github.com>
2025-07-22 23:29:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def home():
+    return "API activa"
+
 @app.route("/limpiar", methods=["POST"])
 def limpiar():
     data = request.get_json()
     texto = data.get("texto", "")
     
-    # Procesamiento de prueba (luego lo cambiás por lo tuyo)
-    limpio = texto.upper()
+    limpio = texto.upper()  # Lógica de prueba
 
     return jsonify({"texto_limpio": limpio})
 #mejora: limpieza robusta de texto
